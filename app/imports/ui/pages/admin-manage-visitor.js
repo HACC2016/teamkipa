95ba5f834edb7b03f193bacaadfef81b85c5ac98
Philip Johnson <johnson@hawaii.edu>
2016-09-21 17:11:53 -1000

    complete refactoring of manage visitor page.

--- app/imports/ui/pages/admin-manage-visitor.js
+++ app/imports/ui/pages/admin-manage-visitor.js
@@ -1,13 +1,13 @@
 import { Template } from 'meteor/templating';
 
-Template.Admin_Home_Page.onCreated(function onCreated() {
+Template.Admin_Manage_Visitor_Page.onCreated(function onCreated() {
   // placeholder: typically you will put global subscriptions here if you remove the autopublish package.
 });
 
-Template.Admin_Home_Page.helpers({
+Template.Admin_Manage_Visitor_Page.helpers({
   // placeholder: if you display dynamic data, you will put your template helpers here.
 });
 
-Template.Admin_Home_Page.events({
+Template.Admin_Manage_Visitor_Page.events({
   // placeholder: if you have a form, handle the associated events here.
 });
--- app/server/main.js
+++ app/server/main.js
@@ -1,10 +1,5 @@
 import '/imports/startup/server';
 import '/imports/startup/both';
-<<<<<<< HEAD
-import '/imports/api/stuff';
-
-
 import '/imports/api/timeslot';
-=======
 import '/imports/api/visitor';
->>>>>>> master
+
