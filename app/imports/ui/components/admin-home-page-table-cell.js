def164a6733e0c08cea42b8a23d6083b11dd5d46
Philip Johnson <johnson@hawaii.edu>
2016-09-22 06:24:34 -1000

    At a stable point for the admin reservation table.

--- app/imports/ui/components/admin-home-page-table-cell.js
+++ app/imports/ui/components/admin-home-page-table-cell.js
@@ -4,9 +4,15 @@ Template.Admin_Home_Page_Table_Cell.onCreated(function onCreated() {
   // placeholder: typically you will put global subscriptions here if you remove the autopublish package.
 });
 
-Template.Admin_Manage_Visitor_Page.helpers({
+Template.Admin_Home_Page_Table_Cell.helpers({
+  getID: function getID(visitor) {
+    return `${visitor}-ID`;
+  },
+  getName: function getName(visitor) {
+    return `${visitor}`;
+  },
 });
 
-Template.Admin_Manage_Visitor_Page.events({
+Template.Admin_Home_Page_Table_Cell.events({
   // placeholder: if you have a form, handle the associated events here.
 });
