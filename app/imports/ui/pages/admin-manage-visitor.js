743ae3412c3c7360f4a53b637cd2785dffcf9ec1
Cam Moore <cammymoore07@gmail.com>
2016-09-23 07:14:29 -1000

    Fixed typo in text message.

--- app/imports/ui/pages/admin-manage-visitor.js
+++ app/imports/ui/pages/admin-manage-visitor.js
@@ -72,7 +72,7 @@ Template.Admin_Manage_Visitor_Page.events({
     const visitor = getvisitor();
     if (typeof visitor !== 'undefined') {
       Visitors.update({ _id: visitor._id }, { $set: { state: 'authorized' } });
-      const message = `From OOOC, ${visitor.firstname} you have been approved to visit ${visitor.detaineefirstname} ${visitor.detaineelastname}`;
+      const message = `From OCCC Visitation, ${visitor.firstname} you have been approved to visit ${visitor.detaineefirstname} ${visitor.detaineelastname}`;
       Meteor.call('sendTextMessage', visitor.phonenumber, message);
       FlowRouter.go('/admin-home');
     }
